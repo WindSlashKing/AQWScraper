5b056229bd554ce00387ea15400ba895d57675d9
WindSlashKing <teemoop2354@gmail.com>
2023-02-19 17:15:18 +0200

    added asworld.org

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import zeroaq
 import miraclelegendz
+import asworld
 
 def main():
 
     print("(1) Scrape zeroaq.com")
     print("(2) Scrape miraclelegendz.online")
+    print("(3) Scrape as-world.org")
     choice = input("> ")
     
     if "1" in choice:
         zeroaq.scrape_no_requirements_quests()
     elif "2" in choice:
         miraclelegendz.scrape_no_requirements_quests()
+    elif "3" in choice:
+        asworld.scrape_no_requirements_quests()
     else:
         print("Invalid input")
         input("Press enter to exit...")
